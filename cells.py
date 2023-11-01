0240b79d092496e782a7cf65cad39dda3aaa5f0b
Allie Rose <roseall@oregonstate.edu>
2023-11-01 07:42:29 -0700

    debug icon updates

--- cells.py
+++ cells.py
@@ -1,75 +1,49 @@
 import tkinter as tk
-from PIL import Image, ImageTk
-
-# icons for cells
-
-
 
 class Cell ():
     def __init__(self, root, game, row, column) -> None:
-        self._icons = {}
-        self.init_icons()
-
         # construct button object
-        self._button = tk.Button(root, state='active', image=self._icons['blank'])
+        self._button = tk.Button(root, image=game.get_icon('blank'))
         self._button.bind('<ButtonRelease-1>', self.left_click) # can change activation to <Button-1> if on click rather than click+release
         self._button.bind('<Button-3>', self.right_click)
         self._button.grid(row=row, column=column)
         self._game = game # reference parent game
         self._row = row
         self._column = column
-        self._is_bomb = False # by default
+        self._is_mine = False # by default
         self._flagged = False
     
-
-    def init_icons(self):
-        """
-        Initializes all icons for later display on cells.
-        """
-        self._icons['blank'] = tk.PhotoImage(file='icons/blank.png').subsample(4)
-        self._icons['mine'] = tk.PhotoImage(file='icons/mine.png').subsample(4)
-        self._icons['flag'] = tk.PhotoImage(file='icons/flag.png').subsample(4)
-        self._icons[1] = tk.PhotoImage(file='icons/1.png').subsample(4)
-        self._icons[2] = tk.PhotoImage(file='icons/2.png').subsample(4)
-        self._icons[3] = tk.PhotoImage(file='icons/3.png').subsample(4)
-        self._icons[4] = tk.PhotoImage(file='icons/4.png').subsample(4)
-        self._icons[5] = tk.PhotoImage(file='icons/5.png').subsample(4)
-        self._icons[6] = tk.PhotoImage(file='icons/6.png').subsample(4)
-        self._icons[7] = tk.PhotoImage(file='icons/7.png').subsample(4)
-        self._icons[8] = tk.PhotoImage(file='icons/8.png').subsample(4)
-        self._icons[9] = tk.PhotoImage(file='icons/9.png').subsample(4)
 
     def left_click(self, event):
         """
         Event handler for left-click of a button.
         """
-        if self._flagged:
-            return
-
-        event.widget["state"] = "disabled" 
-        if self._is_bomb:
-            self._button["image"] = self._icons['mine']
+        event.widget["state"] = tk.DISABLED
+        event.widget["bg"] = "gray64"
+        event.widget["relief"] = tk.SUNKEN
+        if self._is_mine:
+            self._button["image"] = self._game.get_icon('mine')
             self._game.lose()
-        elif event.widget["text"] == "": # ** may need to change later
-            self._game.cell_clicked()
-            event.widget["text"] = "A"
+        else: 
+            self._button["image"] = self._game.adjacent_bomb_count(self._row, self._column)
+            self._game.cell_clicked(self._row, self._column)
     
     def right_click(self, event):
         """
         Event handler for right-click of a button.
         """
-        
-        if self._flagged:
+        if self._flagged: # unflag cell
             event.widget["state"] = "active"
-            self._button["image"] = self._icons['blank']
-        elif event.widget["state"] == "active":
+            self._button["image"] = self._game.get_icon('blank')
+            self._flagged = False
+        elif event.widget["state"] != "disabled":
             event.widget["state"] = "disabled"
-            self._button["image"] = self._icons['flag']
+            self._button["image"] = self._game.get_icon('flag')
             self._flagged = True
     
     def set_bomb(self):
         """
         Sets the cell to be a bomb
         """
-        self._is_bomb = True
+        self._is_mine = True
             
--- game.py
+++ game.py
@@ -2,12 +2,15 @@ import tkinter as tk
 from stopwatch import Timer
 from cells import Cell
 import random
+from PIL import Image, ImageTk
 
 class Game ():
     def __init__(self, root, timer, rows, columns) -> None:
         self._timer = timer
         self._frame = tk.Frame(root, padx=20, pady=20)
         self._frame.pack()
+        self._icons = {}
+        self._init_icons()
         self._cells = []
         self._bombs = []
         self._clicked_cells = 0
@@ -17,7 +20,30 @@ class Game ():
                 row.append(Cell(self._frame, self, i, j))
             self._cells.append(row)
 
-    def _start_game(self):
+    def _init_icons(self):
+        """
+        Initializes all icons for later display on cells.
+        """
+        self._icons['blank'] = tk.PhotoImage(file='icons/blank.png').subsample(4)
+        self._icons['mine'] = tk.PhotoImage(file='icons/mine.png').subsample(4)
+        self._icons['flag'] = tk.PhotoImage(file='icons/flag.png').subsample(4)
+        self._icons[1] = tk.PhotoImage(file='icons/1.png').subsample(4)
+        self._icons[2] = tk.PhotoImage(file='icons/2.png').subsample(4)
+        self._icons[3] = tk.PhotoImage(file='icons/3.png').subsample(4)
+        self._icons[4] = tk.PhotoImage(file='icons/4.png').subsample(4)
+        self._icons[5] = tk.PhotoImage(file='icons/5.png').subsample(4)
+        self._icons[6] = tk.PhotoImage(file='icons/6.png').subsample(4)
+        self._icons[7] = tk.PhotoImage(file='icons/7.png').subsample(4)
+        self._icons[8] = tk.PhotoImage(file='icons/8.png').subsample(4)
+        self._icons[9] = tk.PhotoImage(file='icons/9.png').subsample(4)
+    
+    def get_icon(self, name):
+        """
+        Takes a string and returns the PhotoImage icon.
+        """
+        return self._icons[name]
+
+    def _start_game(self, row, column):
         """
         Triggers the start of the game by starting the game's timer and placing bombs on the board.
         """
@@ -26,7 +52,7 @@ class Game ():
         # place bombs
         for i in range(10):
             bomb_location = (random.randint(0, 8), random.randint(0,8))
-            while bomb_location in self._bombs:
+            while bomb_location in self._bombs or bomb_location == (row, column):
                 bomb_location = (random.randint(0, 8), random.randint(0,8))
             self._bombs.append(bomb_location)
             self._cells[bomb_location[0]][bomb_location[1]].set_bomb()
@@ -34,14 +60,14 @@ class Game ():
             # TESTING !!!
             print(bomb_location) 
     
-    def cell_clicked(self):
+    def cell_clicked(self, row, column):
         """
         Should be called when a non-bomb cell is clicked. Updates the count of selected cells and triggers
         game start (if first cell clicked) or win state (if all non-bomb cells clicked).
         """
         self._clicked_cells += 1
         if self._clicked_cells == 1:
-            self._start_game()
+            self._start_game(row, column)
         elif self._clicked_cells == 71: # all non-bomb cells clicked
             self.win()
         
@@ -49,6 +75,20 @@ class Game ():
         if self._clicked_cells % 10 == 0:
             print(self._clicked_cells)
     
+    def adjacent_bomb_count(self, row, column):
+        """
+        Returns the number of adjacent bombs to the given row, col.
+        """
+        count = 0
+        for i in range(row-1, row+2):
+            for j in range(column-1, column+2):
+                if (i,j) in self._bombs:
+                    count += 1
+        if count > 0:
+            return self._icons[count]
+        else:
+            return self._icons['blank']
+
     def win(self):
         """
         Performs the win-state functions of the game, including stopping the timer, making all cells inactive,
